1f9e97a36a5ee3b4467a4966605c676c8c3f422f
VIDEO-COMPRESSOR <122086028+VIDEO-COMPRESSOR@users.noreply.github.com>
2023-01-14 09:43:02 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 7498895
 API_HASH = "5204d72f395c291b7257df9631a659ee"
 BOT_TOKEN = "5924430427:AAE8Y67N7V7WNqa0u25kqn_QbAMqg3Mg7tE"
-BOT_UN = @TESTSAVERRESTRIVBOT
+BOT_UN = "TESTSAVERRESTRIVBOT"
 #AUTH_USERS = config("AUTH_USERS", default=None)
 #LOG_CHANNEL = config("LOG_CHANNEL", default=None, cast=int)
 #ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None, cast=int)
